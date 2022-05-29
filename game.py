e65a4ede06bd8ab9bd59e1bd299799f46aaa06d2
master defu <nYan_ho3@protonmail.ch>
2022-05-29 23:35:44 +0200

    guessing_game

--- game.py
+++ game.py
@@ -13,23 +13,25 @@ while again == 'y':
     secret = random.randint(1, 9)
     tries = 5
     while tries != 0:
-        guess = int(input('\nYour guess: '))
-        if guess > secret:
-            print('The secret is lower.')
-            tries -= 1
-            print(f'Number of tries -> [{tries}]')
-        elif guess < secret:
-            print('The secret is higher.')
-            tries -= 1
-            print(f'Number of tries -> [{tries}]')
-        else:
-            print(' ____________________________________________')
-            print('|  Correct! Here take this sweet cookie! ^^  |')
-            print('|                                            |')
-            print('|                     @                      |')
-            print('|___________________.---.____________________|')
-            break
-
+        try:
+            guess = int(input('\nYour guess: '))
+            if guess > secret:
+                print('The secret is lower.')
+                tries -= 1
+                print(f'Number of tries -> [{tries}]')
+            elif guess < secret:
+                print('The secret is higher.')
+                tries -= 1
+                print(f'Number of tries -> [{tries}]')
+            else:
+                print(' ____________________________________________')
+                print('|  Correct! Here take this sweet cookie! ^^  |')
+                print('|                                            |')
+                print('|                     @                      |')
+                print('|___________________.---.____________________|')
+                break
+        except ValueError:
+            print('Please enter a number.')
     if tries == 0:
         print('Ah >< You were so close.. Better luck next time!\n')
     again = new_game()
