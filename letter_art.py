b253071ddec473badda1fe4000d87c7c3b45398e
master defu <nYan_ho3@protonmail.ch>
2022-06-01 08:25:06 +0200

    l

---
+++ letter_art.py
@@ -0,0 +1,12 @@
+letter = [5, 2, 5, 2, 2]
+
+for x in letter:
+    output = ''
+    for y in range(x):
+        output += '@'
+    print(output)
+
+'''
+for x_count in letter:
+    print('x' * x_count)
+'''
